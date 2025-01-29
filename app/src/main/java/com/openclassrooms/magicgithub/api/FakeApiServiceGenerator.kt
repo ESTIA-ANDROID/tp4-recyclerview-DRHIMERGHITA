e74f09046a5dfab98d12eea227c01836c4c36d07
DRHIMER GHITA <ghita.drhimer@etu.estia.fr>
2025-01-29 17:35:05 +0100

    tp4

--- app/src/main/java/com/openclassrooms/magicgithub/api/FakeApiServiceGenerator.kt
+++ app/src/main/java/com/openclassrooms/magicgithub/api/FakeApiServiceGenerator.kt
@@ -1,40 +1,42 @@
 package com.openclassrooms.magicgithub.api
 
 import com.openclassrooms.magicgithub.model.User
-import java.util.*
+import kotlin.random.Random
 
 object FakeApiServiceGenerator {
 
-    @JvmField
-    var FAKE_USERS = mutableListOf(
-        User("001", "Jake", "https://api.adorable.io/AVATARS/512/1.png"),
-        User("002", "Paul", "https://api.adorable.io/AVATARS/512/2.png"),
-        User("003", "Phil", "https://api.adorable.io/AVATARS/512/3.png"),
-        User("004", "Guillaume", "https://api.adorable.io/AVATARS/512/4.png"),
-        User("005", "Francis", "https://api.adorable.io/AVATARS/512/5.png"),
-        User("006", "George", "https://api.adorable.io/AVATARS/512/6.png"),
-        User("007", "Louis", "https://api.adorable.io/AVATARS/512/7.png"),
-        User("008", "Mateo", "https://api.adorable.io/AVATARS/512/8.png"),
-        User("009", "April", "https://api.adorable.io/AVATARS/512/9.png"),
-        User("010", "Louise", "https://api.adorable.io/AVATARS/512/10.png"),
-        User("011", "Elodie", "https://api.adorable.io/AVATARS/512/11.png"),
-        User("012", "Helene", "https://api.adorable.io/AVATARS/512/12.png"),
-        User("013", "Fanny", "https://api.adorable.io/AVATARS/512/13.png"),
-        User("014", "Laura", "https://api.adorable.io/AVATARS/512/14.png"),
-        User("015", "Gertrude", "https://api.adorable.io/AVATARS/512/15.png"),
-        User("016", "Chloé", "https://api.adorable.io/AVATARS/512/16.png"),
-        User("017", "April", "https://api.adorable.io/AVATARS/512/17.png"),
-        User("018", "Marie", "https://api.adorable.io/AVATARS/512/18.png"),
-        User("019", "Henri", "https://api.adorable.io/AVATARS/512/19.png"),
-        User("020", "Rémi", "https://api.adorable.io/AVATARS/512/20.png")
+    val FAKE_USERS = mutableListOf(
+        User("001", "Jake", "https://avatars.dicebear.com/api/human/1.svg"),
+        User("002", "Paul", "https://avatars.dicebear.com/api/human/2.svg"),
+        User("003", "Phil", "https://avatars.dicebear.com/api/human/3.svg"),
+        User("004", "Guillaume", "https://avatars.dicebear.com/api/human/4.svg"),
+        User("005", "Francis", "https://avatars.dicebear.com/api/human/5.svg"),
+        User("006", "George", "https://avatars.dicebear.com/api/human/6.svg"),
+        User("007", "Louis", "https://avatars.dicebear.com/api/human/7.svg"),
+        User("008", "Mateo", "https://avatars.dicebear.com/api/human/8.svg"),
+        User("009", "April", "https://avatars.dicebear.com/api/human/9.svg"),
+        User("010", "Louise", "https://avatars.dicebear.com/api/human/10.svg"),
+        User("011", "Elodie", "https://avatars.dicebear.com/api/human/11.svg"),
+        User("012", "Helene", "https://avatars.dicebear.com/api/human/12.svg"),
+        User("013", "Fanny", "https://avatars.dicebear.com/api/human/13.svg"),
+        User("014", "Laura", "https://avatars.dicebear.com/api/human/14.svg"),
+        User("015", "Gertrude", "https://avatars.dicebear.com/api/human/15.svg"),
+        User("016", "Chloé", "https://avatars.dicebear.com/api/human/16.svg"),
+        User("017", "April", "https://avatars.dicebear.com/api/human/17.svg"),
+        User("018", "Marie", "https://avatars.dicebear.com/api/human/18.svg"),
+        User("019", "Henri", "https://avatars.dicebear.com/api/human/19.svg"),
+        User("020", "Rémi", "https://avatars.dicebear.com/api/human/20.svg")
     )
 
-    @JvmField
-    var FAKE_USERS_RANDOM = Arrays.asList(
-        User("021", "Lea", "https://api.adorable.io/AVATARS/512/21.png"),
-        User("022", "Geoffrey", "https://api.adorable.io/AVATARS/512/22.png"),
-        User("023", "Simon", "https://api.adorable.io/AVATARS/512/23.png"),
-        User("024", "André", "https://api.adorable.io/AVATARS/512/24.png"),
-        User("025", "Leopold", "https://api.adorable.io/AVATARS/512/25.png")
+    private val FAKE_USERS_RANDOM = listOf(
+        User("021", "Lea", "https://avatars.dicebear.com/api/human/21.svg"),
+        User("022", "Geoffrey", "https://avatars.dicebear.com/api/human/22.svg"),
+        User("023", "Simon", "https://avatars.dicebear.com/api/human/23.svg"),
+        User("024", "André", "https://avatars.dicebear.com/api/human/24.svg"),
+        User("025", "Leopold", "https://avatars.dicebear.com/api/human/25.svg")
     )
+
+    fun getRandomUser(): User {
+        return FAKE_USERS_RANDOM.random()
+    }
 }
--- app/src/test/java/com/openclassrooms/magicgithub/UserRepositoryTest.kt
+++ app/src/test/java/com/openclassrooms/magicgithub/UserRepositoryTest.kt
@@ -1,23 +1,13 @@
 package com.openclassrooms.magicgithub
 
-import com.openclassrooms.magicgithub.api.FakeApiServiceGenerator.FAKE_USERS
-import com.openclassrooms.magicgithub.api.FakeApiServiceGenerator.FAKE_USERS_RANDOM
+import com.openclassrooms.magicgithub.api.FakeApiServiceGenerator
 import com.openclassrooms.magicgithub.di.Injection
 import com.openclassrooms.magicgithub.model.User
 import com.openclassrooms.magicgithub.repository.UserRepository
-import org.junit.Assert
-import org.junit.Assert.assertEquals
-import org.junit.Assert.assertTrue
+import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Test
-import org.junit.runner.RunWith
-import org.junit.runners.JUnit4
 
-/**
- * Unit test, which will execute on a JVM.
- * Testing UserRepository.
- */
-@RunWith(JUnit4::class)
 class UserRepositoryTest {
     private lateinit var userRepository: UserRepository
 
@@ -29,30 +19,21 @@ class UserRepositoryTest {
     @Test
     fun getUsersWithSuccess() {
         val usersActual = userRepository.getUsers()
-        val usersExpected: List<User> = FAKE_USERS
-        assertEquals(
-            usersActual,
-            usersExpected
-        )
+        val usersExpected = FakeApiServiceGenerator.FAKE_USERS
+        assertEquals(usersExpected, usersActual)
     }
 
     @Test
     fun generateRandomUserWithSuccess() {
         val initialSize = userRepository.getUsers().size
         userRepository.addRandomUser()
-        val user = userRepository.getUsers().last()
-        assertEquals(userRepository.getUsers().size, initialSize + 1)
-        assertTrue(
-            FAKE_USERS_RANDOM.filter {
-                it.equals(user)
-            }.isNotEmpty()
-        )
+        assertEquals(initialSize + 1, userRepository.getUsers().size)
     }
 
     @Test
     fun deleteUserWithSuccess() {
-        val userToDelete = userRepository.getUsers()[0]
+        val userToDelete = userRepository.getUsers().first()
         userRepository.deleteUser(userToDelete)
-        Assert.assertFalse(userRepository.getUsers().contains(userToDelete))
+        assertFalse(userRepository.getUsers().contains(userToDelete))
     }
 }
