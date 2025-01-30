d2952f82d8508a955f4bb33d382558349dfb186d
DRHIMER GHITA <ghita.drhimer@etu.estia.fr>
2025-01-30 11:37:38 +0100

    Ajout Etape 3

--- app/src/main/java/com/openclassrooms/magicgithub/ui/user_list/ListUserActivity.kt
+++ app/src/main/java/com/openclassrooms/magicgithub/ui/user_list/ListUserActivity.kt
@@ -2,58 +2,79 @@ package com.openclassrooms.magicgithub.ui.user_list
 
 import android.os.Bundle
 import android.util.Log
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.RecyclerView
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.openclassrooms.magicgithub.R
+import com.openclassrooms.magicgithub.databinding.ActivityListUserBinding
 import com.openclassrooms.magicgithub.di.Injection.getRepository
 import com.openclassrooms.magicgithub.model.User
 
 class ListUserActivity : AppCompatActivity(), UserListAdapter.Listener {
-    // FOR DESIGN ---
-    lateinit var recyclerView: RecyclerView
-    lateinit var fab: FloatingActionButton
 
-    // FOR DATA ---
+    private lateinit var binding: ActivityListUserBinding
     private lateinit var adapter: UserListAdapter
 
-    // OVERRIDE ---
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_list_user)
-        configureFab()
-        configureRecyclerView()
+        binding = ActivityListUserBinding.inflate(layoutInflater).apply { setContentView(root) }
+
+        setupFab()
+        setupRecyclerView()
     }
 
     override fun onResume() {
         super.onResume()
-        loadData()
+        refreshUserList()
     }
 
-    // CONFIGURATION ---
-    private fun configureRecyclerView() {
-        recyclerView = findViewById(R.id.activity_list_user_rv)
-        adapter = UserListAdapter(this)
-        recyclerView.adapter = adapter
+    private fun setupRecyclerView() {
+        adapter = UserListAdapter(this).apply { binding.activityListUserRv.adapter = this }
+        enableSwipeGesture()
+        enableDragAndDrop()
     }
 
-    private fun configureFab() {
-        fab = findViewById(R.id.activity_list_user_fab)
-        fab.setOnClickListener(View.OnClickListener { view: View? ->
+    private fun setupFab() {
+        binding.activityListUserFab.setOnClickListener {
             getRepository().addRandomUser()
-            loadData()
-        })
+            refreshUserList()
+        }
     }
 
-    private fun loadData() {
+    private fun refreshUserList() {
         adapter.updateList(getRepository().getUsers())
     }
 
-    // ACTIONS ---
     override fun onClickDelete(user: User) {
-        Log.d(ListUserActivity::class.java.name, "User tries to delete a item.")
+        Log.d(TAG, "User is deleting an item.")
         getRepository().deleteUser(user)
-        loadData()
+        refreshUserList()
+    }
+
+    private fun enableSwipeGesture() {
+        val swipeCallback = object : ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT) {
+            override fun onMove(recyclerView: RecyclerView, viewHolder: RecyclerView.ViewHolder, target: RecyclerView.ViewHolder): Boolean = false
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                val user = adapter.getUsers()[viewHolder.adapterPosition].apply { isActive = !isActive }
+                adapter.notifyItemChanged(viewHolder.adapterPosition)
+            }
+        }
+        ItemTouchHelper(swipeCallback).attachToRecyclerView(binding.activityListUserRv)
+    }
+
+    private fun enableDragAndDrop() {
+        val dragCallback = object : ItemTouchHelper.SimpleCallback(ItemTouchHelper.UP or ItemTouchHelper.DOWN, 0) {
+            override fun onMove(recyclerView: RecyclerView, viewHolder: RecyclerView.ViewHolder, target: RecyclerView.ViewHolder): Boolean {
+                adapter.swapItems(viewHolder.adapterPosition, target.adapterPosition)
+                return true
+            }
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {}
+        }
+        ItemTouchHelper(dragCallback).attachToRecyclerView(binding.activityListUserRv)
+    }
+
+    companion object {
+        private val TAG = ListUserActivity::class.java.simpleName
     }
 }
